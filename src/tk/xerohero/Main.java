54d4cc99c52a74d475bddc1b5f30e50997fad618
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2020-11-06 22:52:20 +0000

    Add support for Maven to import dependencies

--- src/tk/xerohero/Main.java
+++ src/tk/xerohero/Main.java
@@ -1,15 +1,58 @@
 package tk.xerohero;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
 import javax.swing.*;
 import java.io.File;
 import java.text.ParseException;
 
+import static javafx.application.Application.launch;
 import static tk.xerohero.Calculations.toGB;
 import static tk.xerohero.GUI.layoutFrame;
 import static tk.xerohero.OsFunctions.determineOsCalculation;
 import static tk.xerohero.OsFunctions.getSpaceReadings;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
 
-public class Main {
+
+
+public class Main extends Application{
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        //Draw a line
+
+        Line line = new Line();
+        line.setStartX(-10);
+        line.setStartY(-0.5);
+        line.setEndX(2);
+        line.setEndY(2.5);
+        Group group = new Group(line);
+        Group root = new Group(line);
+        Scene scene = new Scene(group,600, 300);
+        scene.setFill(Color.BLUEVIOLET);
+
+        Scene rootScene = new Scene(root, 200, 200 );
+        primaryStage.setTitle("My first JavaFX Application");
+        primaryStage.setScene(rootScene);
+        Stage secondStage = new Stage();
+        secondStage.setTitle("Second stage");
+
+        primaryStage.show();
+        secondStage.show(); //Can apply this to my DSAnalyz Project
+    }
+
     static JTextField total, free, used;
 
     static double freeBytes = toGB(new File("/").getFreeSpace());
@@ -25,8 +68,8 @@ public class Main {
         getSpaceReadings(freeBytes, totalBytes, usedBytes);
 
         layoutFrame(frame, freeBytes, usedBytes, total, free, used);
+        launch(args);
     }
-
 
     @Override
     public String toString() {
